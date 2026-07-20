0e65cd93028c4a92dea14ac0aeec659636b520c4
Manuel Rojo Vivot <mrvivot@gmail.com>
2026-07-20 17:40:15 -0300

    feat: agrega robots.txt y sitemap.xml (@astrojs/sitemap)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://brvscu.com.ar',
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  integrations: [sitemap()]
 });
